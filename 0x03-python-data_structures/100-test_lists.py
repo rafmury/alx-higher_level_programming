cb0dff528f89d62cce4ac1644c637144569130b2
rafmury <raphaelmurimi469@gmail.com>
2023-06-10 00:01:13 -0700

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/100-test_lists.py
+++ 0x03-python-data_structures/100-test_lists.py
@@ -6,7 +6,7 @@ l = ['hello', 'World']
 lib.print_python_list_info(l)
 del l[1]
 lib.print_python_list_info(l)
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "My string"]
+l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
 lib.print_python_list_info(l)
 l = []
 lib.print_python_list_info(l)
@@ -19,3 +19,4 @@ l.append(4)
 lib.print_python_list_info(l)
 l.pop()
 lib.print_python_list_info(l)
+
