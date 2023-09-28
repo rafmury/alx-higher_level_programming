5fc60fbb5fc0d3a4a8e467b06c33c44c9194fec7
rafmury <kariukimraphael@gmail.com>
2023-09-28 07:16:07 -0700

    cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
-
-# Function to make a GET request
-function make_get_request {
-    local url="$1"
-    local header="X-School-User-Id: 98"
-    
-    curl -s -H "$header" "$url"
-}
-
-# Check if a URL is provided as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# URL from the command line argument
-url="$1"
-
-# Making the GET request and displaying the response body
-echo "Hello School!"
-make_get_request "$url"
-
+#pass a variable in the header of the request
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
