4116a43139e8c1ab5b7b21ff117cd6b2c97b5b71
rafmury <kariukimraphael@gmail.com>
2023-09-29 09:47:07 -0700

    Python Network

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,4 +1,4 @@
-                                                                                                                                                 #!/usr/bin/python3
+#!/usr/bin/python3
 """Python script that takes your GitHub credentials
 (username and password) and uses the GitHub API to display your id
 """
